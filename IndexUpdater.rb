cf5e3c28cfa01b5d90ac9e3c4740a7477b3248d0
hansman <steinbrecher.johann@gmail.com>
2012-12-11 12:02:37 -0800

    Considered correct HTTP responses

--- IndexUpdater.rb
+++ IndexUpdater.rb
@@ -3,17 +3,17 @@
 #Nov. 2012
 
 require 'rsolr'
+SOLR_URL = 'http://localhost:8983/solr'
 
 class IndexUpdater
 
-  def initialize( url )
-	@solr = RSolr.connect :url => url
+  def initialize 
+    @solr = RSolr.connect :url => SOLR_URL
   end
 
   def perform_update( text, update, vendor)
-	@solr.add(type: ['Update', 'ActiveRecord::Base'], class_name: 'Update', id: "Update #{update}", text_textp: text, vendor_id_i: vendor)
-	@solr.commit
-	response = @solr.get 'select', :params => {:q => "*:*"}
+	  @solr.add(type: ['Update', 'ActiveRecord::Base'], class_name: 'Update', id: "Update #{update}", text_textp: text, vendor_id_i: vendor)
+	  @solr.commit	
   end
 
 end
--- index.rb
+++ index.rb
@@ -6,13 +6,18 @@ require 'sinatra'
 require_relative 'DocProcesser'  
 require_relative 'IndexUpdater'
 
-SOLR_URL = 'http://localhost:8983/solr'
+HTTP_SUCCESS = 200
+HTTP_BAD_GATEWAY = 502
 
 get '/vendor/:vendor_id/update/:update_id/document/*' do
-	doc = DocProcesser.new params[:splat].first
-	text = doc.get_text
-	indexer = IndexUpdater.new SOLR_URL
-	a = indexer.perform_update text, params[:update_id], params[:vendor_id] 
-	"#{a}"
+  doc = DocProcesser.new params[:splat].first
+  text = doc.get_text
+  indexer = IndexUpdater.new
+  a = indexer.perform_update text, params[:update_id], params[:vendor_id] 
+  if a['responseHeader']['status'] == 0
+	  status HTTP_SUCCESS
+	else
+	  error HTTP_BAD_GATEWAY		
+  end	
 end
 
