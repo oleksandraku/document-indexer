f533af1af6dd78ccb9d8322aaae3615c1c4fbf57
Ben Woosley <ben.woosley@gmail.com>
2012-12-11 16:01:21 -0800

    Whitespace.

--- lib/index.rb
+++ lib/index.rb
@@ -4,7 +4,7 @@
 
 require 'sinatra'
 require_relative 'airbraker'
-require_relative 'doc_processor'  
+require_relative 'doc_processor'
 require_relative 'index_updater'
 
 HTTP_SUCCESS = 200
@@ -12,11 +12,11 @@ HTTP_BAD_GATEWAY = 502
 
 get '/vendor/:vendor_id/update/:update_id/document/*' do
   text = DocProcesser.get_text params[:splat].first
-  a = IndexUpdater.perform_update text, params[:update_id], params[:vendor_id] 
+  a = IndexUpdater.perform_update text, params[:update_id], params[:vendor_id]
   if a['responseHeader']['status'] == 0
-	  status HTTP_SUCCESS
+    status HTTP_SUCCESS
   else
-    error HTTP_BAD_GATEWAY		
-  end	
+    error HTTP_BAD_GATEWAY
+  end
 end
 
--- lib/index_updater.rb
+++ lib/index_updater.rb
@@ -9,9 +9,9 @@ SOLR = RSolr.connect url: SOLR_URL
 
 class IndexUpdater
   class << self
-    def perform_update( text, post_id, vendor_id)      
-	    SOLR.add(type: ['Post', 'ActiveRecord::Base'], class_name: 'Post', id: "Post #{post_id}", text_textp: text, vendor_id_i: vendor_id)
-	    SOLR.commit	
+    def perform_update( text, post_id, vendor_id)
+      SOLR.add(type: ['Post', 'ActiveRecord::Base'], class_name: 'Post', id: "Post #{post_id}", text_textp: text, vendor_id_i: vendor_id)
+      SOLR.commit
     end
   end
 end
